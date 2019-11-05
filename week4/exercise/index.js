6bda703b371181ae3544d9c1addcbd3ab1d0fcde
avkynsk <avkynsk@gmail.com>
2019-11-05 11:53:47 +0700

    select есть, но не работает

--- week4/exercise/index.js
+++ week4/exercise/index.js
@@ -6,13 +6,45 @@
 function query(collection) {
     //делаем копию коллекции
     let copyCollection = doCopyCollection(collection);
+    //проверка аргументов
+    if(arguments[1] === undefined){
+        return copyCollection;
+    }
+    //принимаеm команды
+    let commads = [].slice.call(arguments, 1),
+        selections = [],
+        filters = {};
+    commads.forEach(function(command){
+        if(command[0] === 'select'){
+            for(let i = 0; i < command[1].length; i++){
+                selections.push(command[1][i]);
+            }
+        } else if(command[0] === 'filter') {
+            //здесь принимаем аргументы filterIn
+        }
+    });
+
+    copyCollection.forEach(function(element){
+        let keys = Object.keys(element);
+        for(let i = 0; i < keys.length; i++){
+            let bool = false;
+            for(let j = 0; j < selections.length; j++){
+                if(keys[i] === selections[j]){
+                    bool = true;
+                }
+            }
+            if(bool == false){
+                delete element.keys[i];//здесь ломается
+            }
+        }
+    });
 }
 
 /**
  * @params {String[]}
  */
 function select() {
-
+    return ['select', [].slice.call(arguments)];
 }
 
 /**
@@ -20,7 +52,7 @@ function select() {
  * @param {Array} values – Массив разрешённых значений
  */
 function filterIn(property, values) {
-
+    return ['filterIn', property, values];
 }
 
 function doCopyCollection(arr){
