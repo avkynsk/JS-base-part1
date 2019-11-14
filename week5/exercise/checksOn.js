91757db78229e6f1434995b8f075ca15f6d9fc21
avkynsk <avkynsk@gmail.com>
2019-11-14 13:05:29 +0700

    week5, starts exercise, create checks for ON

---
+++ week5/exercise/checksOn.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+
+var emitter = require('./index.js');
+
+var notifications = {
+    counter: 0,
+    count: function () {
+        this.counter++;
+    }
+};
+
+var logger = {
+    logs: []
+};
+
+emitter.on('new_notification', notifications, notifications.count);
+
+assert.equal(notifications.counter, 1, 'Получена одна нотификация');
